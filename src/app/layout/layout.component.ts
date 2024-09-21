9b24a8b5f328c444ff0e8d2649bfef8067dc5e01
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2024-09-21 13:40:13 +0600

    cleaned-up layout component

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,35 +1,12 @@
-import { Component, DestroyRef, inject } from '@angular/core';
-import { CommonModule, NgFor, NgIf } from '@angular/common';
-import { FilterOptionsComponent } from '../filter-options/filter-options.component';
-import { SearchOption } from '../interfaces/search-option';
-import { ProductService } from '../services/product.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { map, tap } from 'rxjs';
-import { Product } from '../interfaces/product';
-import { ProductItemComponent } from '../product-item/product-item.component';
+import { Component } from '@angular/core';
+import { DisplayProductsComponent } from '../display-products/display-products.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
-  imports: [NgFor, NgIf, FilterOptionsComponent, ProductItemComponent],
+  imports: [DisplayProductsComponent],
 })
 export class LayoutComponent {
-  productList: Product[] = [];
-
-  private readonly DestroyRef = inject(DestroyRef);
-  private readonly productService = inject(ProductService);
-  
-  onSearchTermChange(searchTerm: Partial<SearchOption>): void {
-    this.fetchItems(searchTerm);
-  }
-
-  private fetchItems(searchTerm: Partial<SearchOption>): void {
-    this.productService.fetchProduct().pipe(
-      tap(value => console.log(value)),
-      map(() => this.productService.filterProducts(searchTerm)),
-      tap(products => this.productList = products),
-      takeUntilDestroyed(this.DestroyRef)).subscribe();
-  }
 }
