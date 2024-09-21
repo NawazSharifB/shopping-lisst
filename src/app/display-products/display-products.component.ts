2b120ef0ec72b38e51a27d8f7b8dfdd61a536ce5
Sharif, Nawaz <u1092743@internal.imsglobal.com>
2024-09-21 13:35:23 +0600

    added display product

---
+++ src/app/display-products/display-products.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-display-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './display-products.component.html',
+  styleUrls: ['./display-products.component.scss']
+})
+export class DisplayProductsComponent {
+
+}
